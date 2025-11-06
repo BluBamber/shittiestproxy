6a102de3bf63653713924733920a671f63f421b3
BluB <117552568+BluBamber@users.noreply.github.com>
2025-11-06 19:45:51 -0300

    Implement basic proxy server in app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+import requests
+
+class ProxyServer:
+    def __init__(self, host='127.0.0.1', port=8080):
+        self.host = host
+        self.port = port
+
+    def handle_request(self, client_socket):
+        request = client_socket.recv(1024)
+        url = request.decode().split()[1].split("://")[1].split("/")[0]
+        response = requests.get(f'http://{url}')
+        client_socket.send(response.content)
+        client_socket.close()
+
+    def start(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.host, self.port))
+        server_socket.listen(5)
+        while True:
+            client_socket, _ = server_socket.accept()
+            threading.Thread(target=self.handle_request, args=(client_socket,)).start()
+
+if __name__ == "__main__":
+    ProxyServer().start()
